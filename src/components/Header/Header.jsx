c4b64c9d3eaf6548c3f24cde7a41eafad2087926
Rufat01 <rufatmustafayev04@gmail.com>
2023-11-01 23:12:55 +0400

    header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
+import styles from '../Header/Header.module.css'
+import Logo from '../../assetss/Kombim.svg'
+import Button from '../Button/Button.jsx'
+
+const links = [
+   {
+      value: 'Ana səhifə'
+   }, 
+   {
+    value: 'Haqqımızda'
+   }, 
+   {
+    value: 'Xidmətlər'
+   }, 
+   {
+    value: 'Əlaqə'
+   }, 
+]
+
 const Header = () => {
   return (
-    <div>
-        Test
-    </div>
+    <header>
+        <div className={styles.logo}>
+            <img src={Logo} alt="" />
+        </div>
+        <ul className={styles.nav_menu}>
+            {links.map(link => (
+               <Link link={link.value} key={link.value} />
+            ))}
+        </ul>
+        <Button bgColor='#DD3142' textColor="#fff">SİZƏ ZƏNG EDƏK</Button>
+    </header>
   )
+}
+
+const Link = ({link}) => {
+  return <a href="#" className={styles.nav_item}>{link}</a>;
 }
 
 export default Header
