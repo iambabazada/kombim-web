9a25d0feafba19b50f0a78ae65bb32fc83dad152
Rufat01 <rufatmustafayev04@gmail.com>
2023-11-02 13:43:12 +0400

    contact section created

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,29 @@
-import '../Button/Button.css'
+import "../Button/Button.css";
 
-const Button = ({ children, bgColor = "transparent", textColor }) => {
-
+const Button = ({
+  children,
+  bgColor = "transparent",
+  textColor,
+  borderColor = "#fff",
+  textSize = "14px"
+}) => {
   const btnStyle = {
     backgroundColor: bgColor,
     borderRadius: "5px",
     color: textColor,
     padding: "15px 20px",
-    fontSize: "14px",
+    fontSize: textSize,
     border: "1px solid #fff",
     cursor: "pointer",
-    transition: '.3s all'
+    transition: ".3s all",
+    borderColor: borderColor,
   };
 
-  return <button className="button" style={btnStyle}>{children}</button>;
+  return (
+    <button className="button" style={btnStyle}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
