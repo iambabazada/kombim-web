1e5fc570f111b1644a637fb50e8b00b4913048c1
Rufat01 <rufatmustafayev04@gmail.com>
2023-11-02 14:30:01 +0400

    kombi image was placed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Contact from './components/Contact/Contact'
+import Image1 from './components/Image1/Image1'
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Hero />
       <Contact />
+      <Image1 />
       <Footer />
     </div>
   )
