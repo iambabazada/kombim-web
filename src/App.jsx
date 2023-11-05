482de3e994ce81cf884a45bc2eff5fb61c6b4e14
Shahriyar <iambabazada@gmail.com>
2023-11-05 23:04:28 +0400

    changes accepted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/Header'
 import React from 'react'
-import Footer from './components/Footer/Footer'
+import Footer from './components/footer/Footer'
 import Hero from './components/Hero/Hero'
 import Contact from './components/Contact/Contact'
 import Image1 from './components/Image1/Image1'
