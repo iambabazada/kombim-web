15200e95028b33b8ad374ac5679a21c957d782fe
Shahriyar <iambabazada@gmail.com>
2023-11-06 23:19:16 +0400

    max width settet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,35 +26,37 @@ const links = [
 
 const Header = () => {
 
-  
-  let navbar = useRef(null);
-  
-    useEffect(() => {
-      window.addEventListener("scroll", () => {
-        let scrollY = Math.round(Math.ceil(window.scrollY))
-        console.log(scrollY);
-        if(scrollY > 90){
-          navbar.current.classList.add("animate__slideInDown")
-        }else{
-          navbar.current.classList.remove("animate__slideInDown")
-        }
-      })
-    }, [])
 
-  
+  let navbar = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      let scrollY = Math.round(Math.ceil(window.scrollY))
+      console.log(scrollY);
+      if (scrollY > 90) {
+        navbar.current.classList.add("animate__slideInDown")
+      } else {
+        navbar.current.classList.remove("animate__slideInDown")
+      }
+    })
+  }, [])
+
+
 
 
   return (
     <header className='header animate__animated' ref={navbar}>
-      <div className="logo">
-        <img src={Logo} alt="" />
+      <div className='header_container'>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <ul className="nav_menu">
+          {links.map(link => (
+            <Link link={link} key={link.value} />
+          ))}
+        </ul>
+        <Button href={"tel:+9940514333931"} bgColor='#DD3142' textColor="#fff">SİZƏ ZƏNG EDƏK</Button>
       </div>
-      <ul className="nav_menu">
-        {links.map(link => (
-          <Link link={link} key={link.value} />
-        ))}
-      </ul>
-      <Button href={"tel:+9940514333931"} bgColor='#DD3142' textColor="#fff">SİZƏ ZƏNG EDƏK</Button>
     </header>
   )
 }
